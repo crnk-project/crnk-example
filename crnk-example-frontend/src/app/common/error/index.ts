1268cf1466aac342a42222dd8afe6165f010ec0f
Remo Meier <remo.meier@adnovum.ch>
2018-01-23 14:22:46 +0100

    moved editor into dedicated ngrx-jsonapi zone

--- crnk-example-frontend/src/app/common/error/index.ts
+++ crnk-example-frontend/src/app/common/error/index.ts
@@ -2,3 +2,4 @@ export * from './common.error.form.component';
 export * from './common.error.internal.component';
 export * from './common.error.nocontent.component';
 export * from './common.error.notauthorized.component';
+export * from './common.error.route';
--- crnk-example-frontend/src/app/common/router/index.ts
+++ crnk-example-frontend/src/app/common/router/index.ts
@@ -1,3 +1,2 @@
-export * from '../error/common.error.route';
 export * from './common.router.navigation';
 export * from './common.router.state.serializer';
